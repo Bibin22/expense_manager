e41e3ea0b46204dd21cb98e14e9340f324f3bc35
bibin22 <bibinjoy82@gmail.com>
2021-04-18 20:53:47 +0530

    login page

--- app_authentication/urls.py
+++ app_authentication/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import RegistrationView
+from django.views.decorators.csrf import csrf_exempt
+from .views import RegistrationView, UsernameValidationView, EmailValidationView
+
 urlpatterns = [
     path('register', RegistrationView.as_view(), name='register'),
+    path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name='validate-username'),
+    path('validate-email', csrf_exempt(EmailValidationView.as_view()), name='validate-email'),
 ]
---
+++ project_expense_manager/static/js/register.js
@@ -0,0 +1,78 @@
+const usernameField=document.querySelector("#usernameField");
+const feedBackArea=document.querySelector(".invalid_feedback");
+const emailField = document.querySelector("#emailField");
+const emailfeedBackArea=document.querySelector(".emailFeedBackArea");
+const usernameSuccessOutput = document.querySelector(".usernameSuccessOutput");
+const passwordField = document.querySelector("#passwordField");
+const showPasswordToggle = document.querySelector(".showPasswordToggle");
+const handleToggleInput = (e) => {
+if(showPasswordToggle.textContent === "SHOW"){
+   showPasswordToggle.textContent = "HIDE";
+   passwordField.setAttribute("type","text")
+}else{
+    showPasswordToggle.textContent = "SHOW";
+
+    passwordField.setAttribute("type","password");
+}
+};
+showPasswordToggle.addEventListener("click", handleToggleInput);
+
+emailField.addEventListener("keyup", (e) => {
+  const emailVal = e.target.value;
+  emailField.classList.remove("is-invalid")
+  emailfeedBackArea.style.display='none';
+  if (emailVal.length > 0) {
+      fetch('/auth/validate-email', {
+      body: JSON.stringify({email: emailVal}),
+      method: 'POST'
+  }).then(res=>res.json())
+     .then(data=>{
+     console.log('data', data)
+     if (data.email_error){
+          emailField.classList.add("is-invalid")
+          emailfeedBackArea.style.display='block';
+          emailfeedBackArea.innerHTML = `<p>${data.email_error}</p>`
+      }
+
+  });
+
+  }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+usernameField.addEventListener("keyup", (e) => {
+
+  const usernameVal = e.target.value;
+  usernameSuccessOutput.style.display="block"
+  usernameSuccessOutput.textContent=`Checking ${usernameVal}`;
+  usernameField.classList.remove("is-invalid")
+  feedBackArea.style.display='none';
+  if (usernameVal.length > 0) {
+      fetch('/auth/validate-username', {
+      body: JSON.stringify({username: usernameVal}),
+      method: 'POST'
+  }).then(res=>res.json())
+     .then(data=>{
+     console.log('data', data)
+     usernameSuccessOutput.style.display="none"
+     if (data.username_error){
+          usernameField.classList.add("is-invalid")
+          feedBackArea.style.display='block';
+          feedBackArea.innerHTML = `<p>${data.username_error}</p>`
+      }
+
+  });
+
+  }
+
+});
